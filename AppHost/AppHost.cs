ce5da03ad1a20101a90cea257e08aabea86f55e3
Mariliis Uusvali <mariliisjaago@hotmail.com>
2026-01-11 09:01:19 +0200

    Add db reference to Aspire
    * but only as reference, Aspire will not manage this db directly

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,8 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.DataGenerator_Api>("api");
+var db = builder.AddConnectionString("DataGenerator");
+
+var api = builder.AddProject<Projects.DataGenerator_Api>("api")
+    .WithReference(db);
 var web = builder.AddProject<Projects.DataGeneratorBlazor>("datageneratorblazor")
     .WithReference(api)
     .WithExternalHttpEndpoints();
+
+
 
 builder.Build().Run();
